9258d396cee3b97a5e3239c057fda4343df8e8c2
Marcel-Fassou-28 <marcelfassouhaba28@gmail.com>
2025-05-19 01:48:05 +0100

    ajout des animations

--- frontend/src/components/home/InciteSection.jsx
+++ frontend/src/components/home/InciteSection.jsx
@@ -1,27 +1,35 @@
 import {Truck} from 'lucide-react';
 import { CarFront } from 'lucide-react';
-
+import { motion } from 'framer-motion';
 
 function InciteSection() {
   return (
     <div className="flex flex-col md:flex-row mx-0 w-full my-0 h-auto lg:min-h-[500px]">
       <div className='md:w-1/2 w-full md:h-[500px] h-[500px] relative'>
-        <div className='absolute bg-orange-600 h-full w-full flex flex-col text-white justify-center items-start gap-5 p-10'>
+        <motion.div 
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+        className='absolute bg-orange-600 h-full w-full flex flex-col text-white justify-center items-start gap-5 p-10'>
           <CarFront className='size-20' />
           <div className='p-4 flex flex-col gap-3'>
             <h1 className='uppercase font-semibold text-3xl mb-1'>Voitures exclusives</h1>
             <p className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus mollitia nulla sunt porro impedit ratione, distinctio odit. Exercitationem autem aliquam ducimus provident fugiat dolorum maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat molestiae repellat voluptatum, facere perspiciatis odit ipsam eligendi laborum modi beatae?</p>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className='md:w-1/2 md:h-[500px] h-[500px] relative'>
-        <div className='absolute h-full w-full flex flex-col justify-center items-start gap-5 p-10'>
+        <motion.div 
+        initial={{ opacity: 0, x: 30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+        className='absolute h-full w-full flex flex-col justify-center items-start gap-5 p-10'>
           <Truck className='size-20'/>
           <div className='p-4 flex flex-col gap-3'>
             <h1 className='uppercase font-semibold text-3xl mb-1'>Besoin de mobilité?</h1>
             <p className='text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus mollitia nulla sunt porro impedit ratione, distinctio odit. Exercitationem autem aliquam ducimus provident fugiat dolorum maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat molestiae repellat voluptatum, facere perspiciatis odit ipsam eligendi laborum modi beatae?</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
--- frontend/src/components/home/PresentationSection.jsx
+++ frontend/src/components/home/PresentationSection.jsx
@@ -1,28 +1,46 @@
 import Description from '../common/Description'
 import DescriptionPicture from './../../assets/Vehicule/description.webp'
+import { delay, motion } from 'framer-motion';
 
 function PresentationSection() {
   return (
     <div className="bg-white pt-0 mt-0 px-16 pb-8">
-      <div className="py-10">
+      <motion.div 
+      className="py-10"
+      initial={{ opacity: 0, y: -40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.7, delay: 0.3 }}
+      >
         <h1 className="uppercase text-4xl font-semibold pb-2">
             <span className="text-orange-500 tracking-wide">rent car</span>: louez l'excellence, Vivez le confort.<br />
             Découvrez le luxe, le confort et la liberté dès votre première réservation.
         </h1>
-        <hr className="w-24 h-1 my-3 bg-black border-0 rounded-sm md:my-10"/>
-      </div>
+        
+        <motion.hr className="w-24 h-1 my-3 bg-black border-0 rounded-sm md:my-10"
+        initial={{ width: 0 }}
+        animate={{ width: "6rem" }}
+        transition={{ duration: 0.8, delay: 0.3 }}/>
+      </motion.div>
 
       {/* Description de l'entreprise */}
       <div className="flex flex-col gap-3 md:gap-2 md:flex-row align-top">
-        <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 gap-2">
+        <motion.iv 
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 gap-2">
             <div className="col-span-2 p-4 text-justify font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum cumque quisquam repellendus nostrum. Necessitatibus sunt delectus similique excepturi animi debitis dolorem illo quibusdam rerum minus deleniti laudantium quasi expedita, dolores architecto explicabo adipisci ducimus quaerat maxime praesentium labore quam assumenda enim. Unde veritatis vel officia repellat quis dolores praesentium.</div>
             <Description title='qualité' content='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magni expedita ducimus, ratione nulla consequuntur!' />
             <Description title='flexibilité' content='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magni expedita ducimus, ratione nulla consequuntur!' />
             <Description title='disponibilité' content='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magni expedita ducimus, ratione nulla consequuntur!' />
-        </div>
-        <div className='p-4'>
+        </motion.iv>
+        <motion.div 
+        className='p-4'
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}>
             <img src={DescriptionPicture} alt="" />
-        </div>
+        </motion.div>
       </div>
     </div>
   )
